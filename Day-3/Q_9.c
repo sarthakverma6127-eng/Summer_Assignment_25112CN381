f2560f17c53e7fd22c167e3164583461b0400156
Sarthak Verma <sarthakverma6127@gmail.com>
2026-06-03 20:19:50 +0530

    Day-3 assignment uploaded.

---
+++ Day-3/Q_9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // 0 and 1 are not prime numbers
+    if (num <= 1) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
